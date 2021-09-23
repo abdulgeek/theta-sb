a4a5c34376cd2e634384c448509a2823680f71cc
Abdul <abdul.rahman@geekseat.com.au>
2021-09-23 17:17:07 +0700

    first commit

---
+++ src/main/java/com/geekseat/theta/ThetaSbApplication.java
@@ -0,0 +1,13 @@
+package com.geekseat.theta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThetaSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekseat/theta/ThetaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekseat.theta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThetaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
